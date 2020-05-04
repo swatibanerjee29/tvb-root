605014391e90b92915d56f8c2419c4604608a128
bvalean <bogdan.valean@codemart.ro>
2020-05-04 12:24:20 +0300

    TVB-2523 Add conda env setup

--- tvb_build/mac_conda_env.sh
+++ tvb_build/mac_conda_env.sh
@@ -5,27 +5,26 @@ conda create -y --name mac-distribution python=3 nomkl numba scipy numpy network
 conda install -y --name mac-distribution pytest pytest-cov pytest-benchmark pytest-mock matplotlib-base
 conda install -y --name mac-distribution psycopg2 pytables scikit-image==0.14.2 simplejson cherrypy docutils werkzeug==0.16.1
 conda install -y --name mac-distribution -c conda-forge jupyterlab flask gevent
-conda init
-conda activate mac-distribution
-pip install --upgrade pip
-pip install h5py>=2.10 formencode cfflib jinja2 nibabel sqlalchemy==1.1.14 sqlalchemy-migrate==0.11.0 allensdk
-pip install tvb-gdist typing BeautifulSoup4 subprocess32 flask-restplus python-keycloak mako
-pip install py2app docutils apscheduler pyobjc
-pip install --upgrade setuptools
-pip install --upgrade distribute
+
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade pip
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py>=2.10 formencode cfflib jinja2 nibabel sqlalchemy==1.1.14 sqlalchemy-migrate==0.11.0 allensdk
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install tvb-gdist typing BeautifulSoup4 subprocess32 flask-restplus python-keycloak mako
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install py2app docutils apscheduler pyobjc
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade setuptools
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade distribute
 
 # Install TVB
 cd ../framework_tvb
-python setup.py develop --no-deps
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/python setup.py develop --no-deps
 
 cd ../scientific_library
-python setup.py develop
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/python setup.py develop
 
 cd ../scientific_library/contrib
-python setup.py develop --no-deps
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/python setup.py develop --no-deps
 
 cd ../../tvb_bin
-python setup.py develop
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/python setup.py develop
 
 cd ../tvb_build
-python setup.py develop --no-deps
+/WORK/anaconda3/anaconda3/envs/mac-distribution/bin/python setup.py develop --no-deps
