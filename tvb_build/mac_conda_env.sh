f855759c8ac108863755332597752c5f5f74e9e6
bvalean <bogdan.valean@codemart.ro>
2020-05-05 10:43:01 +0300

    TVB-2523 Conda try to install pillow from conda-forge

--- tvb_build/mac_conda_env.sh
+++ tvb_build/mac_conda_env.sh
@@ -4,8 +4,7 @@
 conda create -y --name mac-distribution python=3.7 nomkl numba scipy numpy networkx scikit-learn cython pip numexpr psutil
 conda install -y --name mac-distribution pytest pytest-cov pytest-benchmark pytest-mock matplotlib-base
 conda install -y --name mac-distribution psycopg2 pytables scikit-image==0.14.2 simplejson cherrypy docutils werkzeug==0.16.1
-conda install -y --name mac-distribution -c conda-forge jupyterlab flask gevent
-conda install -y --name mac-distribution -f pillow
+conda install -y --name mac-distribution -c conda-forge jupyterlab flask gevent pillow
 
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade pip
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install h5py>=2.10 formencode cfflib jinja2 nibabel sqlalchemy==1.1.14 sqlalchemy-migrate==0.11.0 allensdk
@@ -13,6 +12,7 @@ conda install -y --name mac-distribution -f pillow
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install py2app docutils apscheduler pyobjc
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade setuptools
 /WORK/anaconda3/anaconda3/envs/mac-distribution/bin/pip install --upgrade distribute
+
 # Add an empty __init__.py in conda_env/python3.7/site-packages/PyObjCTools/ folder or else py2app won't be able to process this module
 echo "" > /WORK/anaconda3/anaconda3/envs/mac-distribution/lib/python3.7/site-packages/PyObjCTools/__init__.py
 ln -s /WORK/anaconda3/anaconda3/envs/mac-distribution/lib/libpython3.7m.dylib /WORK/anaconda3/anaconda3/envs/mac-distribution/lib/libpython3.7.dylib
